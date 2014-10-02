54504b75e1a57c6c68193bf895378a1307ca67d0
Endy Muhardin <endy.muhardin@gmail.com>
2014-10-02 20:22:44 +0700

    implement query select

--- belajar-jdbc/src/main/java/com/muhardin/endy/belajar/App.java
+++ belajar-jdbc/src/main/java/com/muhardin/endy/belajar/App.java
@@ -1,6 +1,7 @@
 package com.muhardin.endy.belajar;
 
 import com.muhardin.endy.belajar.jdbc.*;
+import java.util.*;
 
 public class App {
     public static void main( String[] args ) {
@@ -21,5 +22,13 @@ public class App {
         
         KontakDao kd = new KontakDao(koneksi);
         kd.simpan(k);
+        
+        List<Kontak> isiDb = kd.cariSemua();
+        for(Kontak kx : isiDb){
+            System.out.println("ID : "+kx.getId());
+            System.out.println("Nama : "+kx.getNama());
+            System.out.println("Email : "+kx.getEmail());
+            System.out.println("No HP : "+kx.getNoHp());
+        }
     }
 }
--- belajar-jdbc/src/main/java/com/muhardin/endy/belajar/jdbc/KontakDao.java
+++ belajar-jdbc/src/main/java/com/muhardin/endy/belajar/jdbc/KontakDao.java
@@ -1,6 +1,7 @@
 package com.muhardin.endy.belajar.jdbc;
 
 import java.sql.*;
+import java.util.*;
 
 public class KontakDao {
     private KoneksiDatabase koneksi;
@@ -22,5 +23,36 @@ public class KontakDao {
         } catch (SQLException err){
             err.printStackTrace();
         }
+    }
+    
+    public List<Kontak> cariSemua(){
+        List<Kontak> hasil = new ArrayList<Kontak>();
+        
+        try {
+            String sql = "select * from kontak order by nama";
+            Connection c = koneksi.connect();
+            PreparedStatement ps = c.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                Kontak k = konversiResultSet(rs);
+                hasil.add(k);
+            }
+            
+            koneksi.disconnect(c);
+        } catch (SQLException err){
+            err.printStackTrace();
+        }
+        
+        return hasil;
+    }
+    
+    private Kontak konversiResultSet(ResultSet rs) throws SQLException {
+        Kontak k = new Kontak();
+        k.setId(rs.getInt("id"));
+        k.setNama(rs.getString("nama"));
+        k.setEmail(rs.getString("email"));
+        k.setNoHp(rs.getString("no_hp"));
+        return k;
     }
 }
