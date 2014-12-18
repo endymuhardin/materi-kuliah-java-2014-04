be7685d777b4ba37ba3fc6119c10be079ef47814
Endy Muhardin <endy.muhardin@gmail.com>
2014-12-18 20:12:45 +0700

    implement servlet untuk simpan data

---
+++ belajar-aplikasi-web-20/server/aplikasi-mahasiswa-rest/src/main/java/com/muhardin/endy/belajar/servlet/SimpanMahasiswaServlet.java
@@ -0,0 +1,31 @@
+package com.muhardin.endy.belajar.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.common.io.CharStreams;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class SimpanMahasiswaServlet extends HttpServlet {
+
+    private MahasiswaDao md = new MahasiswaDao();
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String data = CharStreams.toString(req.getReader());
+        System.out.println("Data : "+data);
+        
+        Mahasiswa m = new ObjectMapper().readValue(data, Mahasiswa.class);
+        System.out.println("ID : "+m.getId());
+        System.out.println("NPM : "+m.getNpm());
+        System.out.println("Nama : "+m.getNama());
+        System.out.println("Email : "+m.getEmail());
+        System.out.println("Tanggal Lahir : "+m.getTanggalLahir());
+    }
+}
